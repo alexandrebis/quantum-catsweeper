01614d60b31b0df5e3b288e0a91ec46183dc12cd
pauline.rouvel@hotmail.fr <pauline.rouvel@hotmail.fr>
2020-11-30 21:03:36 +0100

    update qiskit and simplify quantique function

--- qcatsweeper/quantum_logic.py
+++ qcatsweeper/quantum_logic.py
@@ -1,4 +1,4 @@
-from qiskit import QuantumProgram
+from qiskit import *
 from enum import Enum
 from qcatsweeper import qconfig
 
@@ -34,29 +34,32 @@ class TileItems(Enum):
 real_device = False
 shots = 1024
 
-device = 'local_qasm_simulator'
+device = Aer.get_backend('qasm_simulator')
 if real_device:
-    device = 'ibmqx4'
-
-Q_program = QuantumProgram()
+    IBMQ.save_account(qconfig.APItoken)
+    IBMQ.load_account()
+    provider = IBMQ.get_provider(hub='ibm-q')
+    device = provider.get_backend('ibmq_qasm_simulator')
 
 
 # qiskit.register(qconfig.APItoken, qconfig.config["url"])
 
 
-def get_one_or_zero(grid_script, q, c, index):
-    global Q_program
-    # measuring qubit and finding which value has the most outcomes
-    grid_script.measure(q[index], c[index])
-    results = Q_program.execute(
-        ["gridScript"], backend=device, shots=shots, timeout=1800)
-    re = results.get_counts("gridScript")
-    d1 = list(map(lambda x: (x[0], x[1], x[0].count('0')), re.items()))
-    d2 = sorted(d1, key=lambda x: x[2], reverse=True)
+def get_one_or_zero(grid_script, q, c):
 
-    print(d2)
-    if d2[0][1] > d2[1][1]:
+    # measuring qubit and finding which value has the most outcomes
+    grid_script.measure(q, c)
+    results = execute(grid_script, device, shots=shots).result()
+    results_counts = results.get_counts(grid_script)
+    print("results_counts", results_counts)
+
+    if len(results_counts) == 1:
+        print("return 0")
         return 0
+    if results_counts['0'] > results_counts['1']:
+        print("return 0")
+        return 0
+    print("return 1")
     return 1
 
 
@@ -103,63 +106,18 @@ def onclick(clicked_tile, num_clicks):
     clicked_tile: tile type of the clicked tile
     num_click: number of times a group has been clicked
     """
-    q = Q_program.create_quantum_register("q", 5)
-    c = Q_program.create_classical_register("c", 5)
-    gridScript = Q_program.create_circuit("gridScript", [q], [c])
+    q = QuantumRegister(1)
+    c = ClassicalRegister(1)
+    gridScript =  QuantumCircuit(q, c, name='gridScript')
 
     if (clicked_tile == TileItems.BOMB_UNEXPLODED):
         # hadamard gate applied to bomb qubit
-        gridScript.h(q[0])
+        gridScript.h(q)
 
         # if there are more 1 hits then the bomb expodes and the game is lost
-        if get_one_or_zero(gridScript, q, c, 0) == 1:
+        if get_one_or_zero(gridScript, q, c) == 1:
             return TileItems.BOMB_EXPLODED
         return TileItems.BOMB_DEFUSED
 
-    elif (clicked_tile == TileItems.GROUP1 or clicked_tile == TileItems.GROUP2):  # 1 click
-        # half not gate applied to the 1 click number tiles
-        gridScript.u3(0.5 * math.pi, 0.0, 0.0, q[1])
-
-        # if more 1 hits then the whole tile group is revealed
-        if get_one_or_zero(gridScript, q, c, 1) == 1:
-            return TileItems.REVEAL_GROUP
-        return TileItems.NEG_EVAL
-
-    elif (clicked_tile == TileItems.GROUP3 or clicked_tile == TileItems.GROUP4):  # 2 clicks
-        if num_clicks == 1:
-            gridScript.u3(0.5 * math.pi, 0.0, 0.0, q[2])
-
-            if get_one_or_zero(gridScript, q, c, 2) == 1:
-                return TileItems.POS_EVAL
-            return TileItems.NEG_EVAL
-
-        elif num_clicks == 2:
-            gridScript.u3(0.5 * math.pi, 0.0, 0.0, q[2])
-
-            if get_one_or_zero(gridScript, q, c, 2) == 1:
-                return TileItems.REVEAL_GROUP
-            return TileItems.NEG_EVAL
-
-    elif (clicked_tile == TileItems.GROUP5 or clicked_tile == TileItems.GROUP6):  # 3 clicks
-        if num_clicks == 1:
-            gridScript.u3(0.5 * math.pi, 0.0, 0.0, q[3])
-
-            if get_one_or_zero(gridScript, q, c, 3) == 1:
-                return TileItems.POS_EVAL
-            return TileItems.NEG_EVAL
-
-        elif num_clicks == 2:
-            gridScript.u3(0.5 * math.pi, 0.0, 0.0, q[3])
-
-            if get_one_or_zero(gridScript, q, c, 3) == 1:
-                return TileItems.POS_EVAL
-            return TileItems.NEG_EVAL
-
-        elif num_clicks == 3:
-            gridScript.u3(0.5 * math.pi, 0.0, 0.0, q[3])
-
-            if get_one_or_zero(gridScript, q, c, 3) == 1:
-                return TileItems.REVEAL_GROUP
-            return TileItems.NEG_EVAL
-
-    return None
+    else:
+        return clicked_tile
